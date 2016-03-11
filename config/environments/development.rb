d43a2b004934d44038ca3671489a8ab5dea8b6e2
dario platania <dariopl91@gmail.com>
2016-03-11 22:13:58 +0100

    invio email funzionante

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,12 +41,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method  = :smtp
 
   config.action_mailer.smtp_settings  = {
-    address:        "smtp.gmail.com",
-    port:           587,
-    domain:         "gmail.com", 
-    authentication: "plain",
-    user_name:      "dariopl91",
-    password:       "darioemarika",
-    enable_starttls_auto: true
-  }
+     address:        "smtp.gmail.com",
+     port:           587,
+     domain:         "gmail.com",
+     authentication: "plain",
+     user_name:      "dariopl91",
+     password:       "darioemarika",
+     enable_starttls_auto: true
+   }
+
 end

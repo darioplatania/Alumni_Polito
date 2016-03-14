1d0db64033692642d98f2e8742d7af03be76b712
dario platania <dariopl91@gmail.com>
2016-03-14 15:34:30 +0100

    implementazione funzionante activeadmin con devise

---
+++ app/admin/home.rb
@@ -0,0 +1,44 @@
+ActiveAdmin.register_page "Home" do
+
+    menu priority: 1
+
+    #HO SOVRASCRITTO IL DASHBOARD DI DEFAULT DI ATIVE ADMIN E ORA CARICA LA HOME CON I MESSAGGI DI BENVENUTO
+    content do
+    div class: "blank_slate_container", id: "dashboard_default_message" do
+      span class: "blank_slate" do
+        span "Benvenuti nel pannello di amministrazione di AlumniPolito"
+        small "Da Sapere:"
+        small "In Modulo ci sono tutti i moduli creati dai partecipanti"      
+      end# end di content
+    end
+
+ #content title: proc{ I18n.t("active_admin.dashboard") } do
+    #div class: "blank_slate_container", id: "dashboard_default_message" do
+      #span class: "blank_slate" do
+        #span I18n.t("active_admin.dashboard_welcome.welcome")
+        #small I18n.t("active_admin.dashboard_welcome.call_to_action")
+      #end
+    #end
+
+    # Here is an example of a simple dashboard with columns and panels.
+    #
+    # columns do
+    #   column do
+    #     panel "Recent Posts" do
+    #       ul do
+    #         Post.recent(5).map do |post|
+    #           li link_to(post.title, admin_post_path(post))
+    #         end
+    #       end
+    #     end
+    #   end
+
+    #   column do
+    #     panel "Info" do
+    #       para "Welcome to ActiveAdmin."
+    #     end
+    #   end
+    # end
+  #end #content
+    end
+end

2122451e9f78495d0b7d7cbadc3b140f614b3689
dario platania <dariopl91@gmail.com>
2016-04-09 12:31:05 +0200

    insert setup_mail.rb

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,12 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.smtp_settings = {
+    address:        "smtp.gmail.com",
+    port:           587,
+    domain:         "gmail.com",
+    authentication: "plain",
+    user_name:      "dariopl91",
+    password:       "darioemarika",
+    enable_starttls_auto: true
+}
